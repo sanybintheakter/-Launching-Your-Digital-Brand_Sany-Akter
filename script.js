f2714375f52e5dcf20b9e5c336f40ec45d221c96
Sany Binthe Akter <sakter@usf.edu>
2026-05-03 14:09:00 -0400

    updated

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-alert ("Hurray, you have found me!!");
+// Portfolio interactions
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Portfolio loaded");
+
+  const projects = document.querySelectorAll(".project");
+
+  projects.forEach((project) => {
+    project.addEventListener("mouseover", () => {
+      project.style.transform = "scale(1.03)";
+      project.style.transition = "0.3s ease";
+    });
+
+    project.addEventListener("mouseout", () => {
+      project.style.transform = "scale(1)";
+    });
+  });
+});
